c726742031af3428c7d9c3097ac0c592078529d9
UI <ui@UIs-MacBook-Pro.local>
2018-11-04 00:09:28 -0400

    Added README.md file

---
+++ README.md
@@ -0,0 +1,15 @@
+# APP LIVE DEMO
+
+# Architecture.
+
+# Sequence diagram of the use cases' interactions.
+
+# How to: build, deploy and access the app.
+
+# API Documentation
+Link to the documentation: https://alexandrursu.github.io/palindrome-swagger/
+
+## Details:
+- ['express-swagger-generator'](https://www.npmjs.com/package/express-swagger-generator) was used to generate swagger doc & ui based on express existing routes.
+- while server is running open http://<app_host>:<app_port>/api-docs in your browser to view the documentation.
+- static HTML version was generated from Swagger schema .json file using https://twskj.github.io/pretty-swag/
--- server/server.js
+++ server/server.js
@@ -1,37 +1,34 @@
+const fs = require("fs");
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 const router = require("./routes/messages");
-
-const app = express();
 const [major, minor] = process.versions.node.split(".").map(parseFloat);
-const expressSwagger = require("express-swagger-generator")(app);
 
-let options = {
+/**
+ * Swagger related configurations.
+ * While server is running Swagger documentation can be accessed
+ * at following route http://{host}:{port}/api-docs/
+ */
+const expressSwagger = require("express-swagger-generator")(app);
+const options = {
   swaggerDefinition: {
     info: {
-      description: "This is a sample server",
+      description: "Swagger API Documentation",
       title: "Swagger",
       version: "1.0.0"
     },
     host: "localhost:3001",
     basePath: "/",
     produces: ["application/json", "application/xml"],
-    schemes: ["http", "https"],
-    securityDefinitions: {
-      JWT: {
-        type: "apiKey",
-        in: "header",
-        name: "Authorization",
-        description: ""
-      }
-    }
+    schemes: ["http", "https"]
   },
   basedir: __dirname, //app absolute path
   files: ["./routes/**/*.js"] //Path to the API handle folder
 };
 expressSwagger(options);
+
 // Cross domain configuration
 let allowCrossDomain = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
@@ -40,6 +37,7 @@ let allowCrossDomain = (req, res, next) => {
 
   next();
 };
+
 // import environmental variables from our variables.env file and handle missing files
 if (fs.existsSync("./variables.env")) {
   require("dotenv").config({ path: "variables.env" });
@@ -53,11 +51,14 @@ mongoose.connect(
   process.env.DATABASE,
   { useNewUrlParser: true }
 );
-mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+
+// Tell Mongoose to use ES6 promises
+mongoose.Promise = global.Promise;
 
 mongoose.connection.on("error", err => {
   console.error(`${err.message}`);
 });
+
 mongoose.connection.on("connected", err => {
   console.log(`Connected to database`);
 });
@@ -77,4 +78,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Start server
 app.use(allowCrossDomain);
 app.use("/", router);
+
 app.listen(3001, () => console.log("Server is listening on port 3001"));
